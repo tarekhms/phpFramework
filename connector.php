a5e8d1f06bb7c2862b866778e9c4fcc8108c83dd
Tarek <tarek.alhomsi@hotmail.com>
2020-02-21 13:13:37 +0100

    default

--- connector.php
+++ connector.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "123";
-$dbname = "fowsion";
+$dbname = "test";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
